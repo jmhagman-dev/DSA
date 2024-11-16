f5d22c27e9b5afee3adad870d529512957465ca0
Manny Hagman <jmhagman.dev@gmail.com>
2024-11-16 00:00:51 -0600

    Rename variables for clarity

--- sorting/insertion-sort/insertion-sort.js
+++ sorting/insertion-sort/insertion-sort.js
@@ -1,15 +1,22 @@
 function insertionSort(nums) {
-  for (let i = 1; i < nums.length; i++) {
-    let currentValue = nums[i]
-    let j = i - 1
-    while (j >= 0 && nums[j] > currentValue) {
-      nums[j + 1] = nums[j]
-      j--
-    }
-    nums[j + 1] = currentValue
+  for (let r = 1; r < nums.length; r++) {
+    let l = r - 1;
+    let rightValue = nums[r];
     
+    while (l >= 0 && nums[l] > rightValue) {
+      let leftValue = nums[l]
+      nums[l + 1] = leftValue;
+      l--;
+    }
+    nums[l + 1] = rightValue;
   }
   return nums;
 }
 
-export default insertionSort
+export default insertionSort;
+
+// [2, 1, 5, 4] currentValue = 1
+//  j  i 
+// j >= 0 true
+// nums[j] > currentValue
+// 
--- sorting/quick-sort/quick-sort.js
+++ sorting/quick-sort/quick-sort.js
@@ -1,11 +1,10 @@
-
 function quicksort(arr, left = 0, right = arr.length - 1) {
   if (left < right) {
-    let pivotIndex = pivot(arr, start, end)
-    // left side
-    quicksort(arr, left, pivotIndex + 1)
-    
-    quicksort(arr, right, pivotIndex - 1)
+    let pivotIndex = pivot(arr, start, end);
+
+    quicksort(arr, left, pivotIndex + 1);
+
+    quicksort(arr, right, pivotIndex - 1);
   }
   return arr;
 }
